d2dfdf89489bbcdc50316f4127a28536c52cda62
SandyaSarathi <46451655+SandyaSarathi@users.noreply.github.com>
2019-09-03 17:29:30 +0530

    Create BST.py

---
+++ BST.py
@@ -0,0 +1,53 @@
+class Node:
+  def __init__(self,data):
+    self.data = data
+    self.left = None
+    self.right= None
+
+class BST:
+  def __init__(self):
+    self.root=None
+  def create(self,val):
+    if self.root==None:
+      self.root=Node(val)
+    else:
+      current=self.root
+      while True:
+        if val<current.data:
+          if current.left:
+            current=current.left
+          else:
+            current.left=Node(val)
+        elif val> current.data:
+          if current.right:
+            current=current.right
+          else:
+            current.right=Node(val)
+        else:
+          break
+
+
+def inOrder(root):
+  if root:
+    inOrder(root.left)
+    print(root.data)
+    inOrder(root.right)
+
+def postOrder(root):
+  if root:
+    postOrder(root.left)
+    postOrder(root.right)
+    print(root.data)
+
+def preOrder(root):
+  if root:
+    print(root.data)
+    preOrder(root.left)
+    preOrder(root.right)
+
+
+tree=BST()
+n=int(input())
+a=[int(i) for i in input().split()]
+for i in range(n):
+  tree.create(a[i])
